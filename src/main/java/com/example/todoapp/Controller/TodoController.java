f49a2a78d5eab12a07995379246b297aa7e90ef5
seonjae <tjswo2292@naver.com>
2024-09-01 21:33:14 +0900

    Controller에 있는 Service로직 분리

--- src/main/java/com/example/todoapp/Controller/TodoController.java
+++ src/main/java/com/example/todoapp/Controller/TodoController.java
@@ -4,52 +4,40 @@ import com.example.todoapp.Service.TodoService;
 import com.example.todoapp.domain.Todo;
 import com.example.todoapp.dto.CreateTodoDto;
 import com.example.todoapp.dto.TodoDto;
-import com.example.todoapp.repository.TodoRepository;
+//import com.example.todoapp.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 // 엔드포인트들 정의하는 곳
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
-    private final TodoRepository todoRepository;
-
     private final TodoService todoService;
 
     @GetMapping("/")
-    public List<Todo> index() {
-        return todoRepository.findAll();
+    public List<Todo> getTodoList() {
+        return todoService.getTodoList();
     }
 
     @Transactional // for dirty checking
     @PatchMapping("/patch-todo")
-    public String patchTodo(@RequestBody TodoDto todoDto) {
-        Optional<Todo> todoContents = todoRepository.findById(todoDto.getTodoId());
-
-        if (todoContents.isPresent()) {
-            Todo todo = todoContents.get();
-            todo.setDescription(todoDto.getTodoTitle());
-
-            return "SUCCESS";
-        } else {
-            return "FAIL";
-        }
+    public void patchTodo(@RequestBody TodoDto todoDto) {
+        todoService.patchTodo(todoDto);
     }
 
     @PostMapping("/create-todo")
-    public ResponseEntity<Todo> createTodo(@RequestBody CreateTodoDto newTodoTitle) {
-        Todo createdTodo = todoService.createTodo(newTodoTitle.getNewTodoTitle());
-        return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
+    public String createTodo(@RequestBody CreateTodoDto newTodoTitle) {
+        todoService.createTodo(newTodoTitle);
+        return "OK";
     }
 
     @DeleteMapping("/delete-todo/{todoId}")
-    public String deleteTodo(@PathVariable String todoId) {
+    public void deleteTodo(@PathVariable String todoId) {
         todoService.deleteTodo(todoId);
-        return "OK";
     }
 }
--- src/main/java/com/example/todoapp/Service/TodoService.java
+++ src/main/java/com/example/todoapp/Service/TodoService.java
@@ -1,9 +1,14 @@
 package com.example.todoapp.Service;
 
 import com.example.todoapp.domain.Todo;
+import com.example.todoapp.dto.CreateTodoDto;
+import com.example.todoapp.dto.TodoDto;
 import com.example.todoapp.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 // controller에서 사용할 서비스 로직들
 @Service
@@ -11,10 +16,24 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
-    public Todo createTodo (String newTodoTitle) {
+    public List<Todo> getTodoList() {
+        return todoRepository.findAll();
+    }
+
+    public void patchTodo(TodoDto todoDto) {
+        // 여기서 어떤 값이 추가되는지
+        Optional<Todo> todoContents = todoRepository.findById(todoDto.getTodoId());
+
+        if (todoContents.isPresent()) {
+            Todo todo = todoContents.get();
+            todo.setDescription(todoDto.getTodoTitle());
+        }
+    }
+
+    public void createTodo (CreateTodoDto newTodoTitle) {
         Todo todo = new Todo();
-        todo.setDescription(newTodoTitle);
-        return todoRepository.save(todo);
+        todo.setDescription(newTodoTitle.getNewTodoTitle());
+        todoRepository.save(todo);
     }
 
     public void deleteTodo(String todoId) {
